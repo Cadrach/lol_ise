804a2c2c53abe8e5df19e7f6358d5b416f6a5c95
Rachid <rachid@actived.fr>
2016-12-13 11:54:32 +0100

    Drop on whole page

--- app/components/set/set-directive.js
+++ app/components/set/set-directive.js
@@ -2,12 +2,18 @@
 
 angular.module('appLolIse.set.set-directive', ['ngFileUpload'])
 
-.directive('setDropper', [function() {
+.directive('setDropper', ['$window', function($window) {
 
     return {
         link: function(scope, elmt){
+
+            /**
+             * **************************************************************************************
+             * SCOPE VARS
+             */
             //Default files list
             scope.files = [];
+            scope.interface = {show: false};
 
             //Watch dropping files
             scope.$watch('files', function () {
@@ -16,19 +22,64 @@ angular.module('appLolIse.set.set-directive', ['ngFileUpload'])
                 }
             });
 
+            /**
+             * **************************************************************************************
+             * LOCAL METHODS
+             */
             //Read the files & generate the sets
             function readFiles(files){
                 //Clear current set
                 scope.sets.splice(0, scope.sets.length);
+                interfaceHide();
 
                 files.forEach(function(file){
                     var reader = new FileReader;
                     reader.onload = function(e){
-                        scope.sets.push(JSON.parse(e.target.result));
+                        var theSet = JSON.parse(e.target.result);
+                        theSet.filepath = file.path;
+                        scope.sets.push(theSet);
                     }
                     reader.readAsText(file);
                 })
             }
+
+            //Hide interface
+            function interfaceShow(){
+                elmt.css('visibility', '');
+                elmt.css('opacity', 1);
+            }
+
+            //Show interface
+            function interfaceHide(){
+                elmt.css('visibility', 'hidden');
+                elmt.css('opacity', 0);
+            }
+
+            /**
+             * **************************************************************************************
+             * WATCHES & EVENTS
+             */
+            //Show/hide dropzone when dragging inside window
+            var lastTarget = null;
+            $window.addEventListener("dragenter", function(e)
+            {
+                lastTarget = e.target;
+                interfaceShow();
+            });
+            $window.addEventListener("dragleave", function(e)
+            {
+                if(e.target === lastTarget)
+                {
+                    interfaceHide();
+                }
+            });
+
+            /**
+             * **************************************************************************************
+             * BOOTSTRAP
+             */
+            interfaceHide();
+
         },
         restrict: 'E',
         scope: {
