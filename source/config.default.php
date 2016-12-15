2e0127676c953d132662699446293f32a63eb07a
Cadrach <cadrach@gmail.com>
2016-12-15 19:27:42 +0100

    PHP JSON generator (all languages)

---
+++ source/config.default.php
@@ -0,0 +1,3 @@
+<?php
+define('RIOT_API_KEY', 'RGAPI-c12afc1e-2d25-4388-959d-b1e9eb797d44');
+define('SECURITY_SEED', 'om1ku5ogZGQ34F6Li6xgu23qu');
---
+++ source/jsonMaker.php
@@ -0,0 +1,33 @@
+<?php
+include 'config.inc.php';
+
+//Security
+if(!isset($_GET['seed']) || $_GET['seed'] != SECURITY_SEED){
+    die('Invalid seed');
+}
+
+function get($url){
+    return json_decode(file_get_contents($url), true);
+}
+
+//Initial vars
+$apiVersion = '1.2';
+$url = "https://global.api.pvp.net/api/lol/static-data/euw/v$apiVersion/";
+$ddragon = "http://ddragon.leagueoflegends.com/cdn/";
+
+//Fetch latest version
+$version = get($url . "versions?api_key=" . RIOT_API_KEY)[0];
+$languages = get($ddragon . 'languages.json');
+
+//Config json
+file_put_contents("config.json", json_encode(['version' => $version, 'langugages' => $languages]));
+
+//Ddragon data, per language
+foreach($languages as $language){
+    $json = [];
+    $json['item'] = get($ddragon . $version . "/data/$language/item.json");
+    $json['champion'] = get($ddragon . $version . "/data/$language/champion.json");
+    $json['language'] = get($ddragon . $version . "/data/$language/language.json");
+    file_put_contents("data_$language.json", json_encode($json));
+}
+echo $version;
