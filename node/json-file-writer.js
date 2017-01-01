c9cefddf14eb66ecab040fecc81e461e5cd734fc
Toilal <toilal.dev@gmail.com>
2017-01-01 11:33:20 +0100

    Add nodeJS script to generate JSON data

---
+++ node/json-file-writer.js
@@ -0,0 +1,28 @@
+// Generates JSON files from API.
+var fs = require('fs');
+
+var load = require('./api-data-loader.js');
+
+var conf = require('./conf.json');
+
+/**
+ * Writes given configuration data into source file.
+ *
+ * @param {ConfigData} configData
+ */
+function configWriter(configData) {
+    fs.writeFile(__dirname + '/../source/config.json', JSON.stringify(configData, null, 2))
+}
+
+/**
+ * Writes given language data into source file.
+ *
+ * @param {String} language
+ * @param {LanguageData} languageData
+ */
+function languageWriter(language, languageData) {
+    fs.writeFile(__dirname + '/../source/data_' + language + '.json', JSON.stringify(languageData, null, 2));
+}
+
+// Write data files.
+load(conf, configWriter, languageWriter);
