8486609a6c504f5787491db8332496c448b0805c
Rachid <rachid@actived.fr>
2017-01-04 18:20:37 +0100

    Now fetch recommended item sets from Riot API

--- source/jsonMaker.php
+++ source/jsonMaker.php
@@ -14,7 +14,7 @@ function get($url){
 $apiVersion = '1.2';
 $url = "https://global.api.pvp.net/api/lol/static-data/euw/v$apiVersion/";
 $ddragon = "http://ddragon.leagueoflegends.com/cdn/";
-
+https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=recommended&api_key=RGAPI-4d0b311d-a616-475f-a012-8c04969b078e
 //Fetch latest version
 $version = get($url . "versions?api_key=" . RIOT_API_KEY)[0];
 $languages = get($ddragon . 'languages.json');
@@ -22,8 +22,12 @@ $languages = get($ddragon . 'languages.json');
 //Config json
 file_put_contents("config.json", json_encode(['version' => $version, 'languages' => $languages, 'generatedOn' => date('Y-m-d H:i:s')]));
 
+//Fetch once full data to get recommended item sets (it is long to fetch)
+$recommended = get($url . "champion?champData=recommended&api_key=" . RIOT_API_KEY)['data'];
+
 //Ddragon data, per language
 foreach($languages as $language){
+    set_time_limit(30); //30s max to fetch the data for a language
     $json = [];
     $json['item'] = get($ddragon . $version . "/data/$language/item.json");
     $json['champion'] = get($ddragon . $version . "/data/$language/champion.json");
@@ -31,6 +35,14 @@ foreach($languages as $language){
     $json['language']['data']['native_ko'] = '한국어';
     $json['language']['data']['Jungle'] = 'Jungle';
     $json['language']['data']['Vision'] = 'Vision';
+
+    //For each champion, fetch the recommended data from the full data
+    foreach($json['champion']['data'] as $key=>&$champ){
+        $champ['recommended'] = $recommended[$key]['recommended'];
+    }
+
     file_put_contents("data_$language.json", json_encode($json));
+    break;
+    sleep(1); //ensure no bypass of rate limit
 }
 echo $version;
