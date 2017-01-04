86013e38c8b5f7f5121d97340c2cb953682291ed
Rachid <rachid@actived.fr>
2017-01-04 10:51:37 +0100

    Store generated date of config.json

--- source/jsonMaker.php
+++ source/jsonMaker.php
@@ -20,7 +20,7 @@ $version = get($url . "versions?api_key=" . RIOT_API_KEY)[0];
 $languages = get($ddragon . 'languages.json');
 
 //Config json
-file_put_contents("config.json", json_encode(['version' => $version, 'languages' => $languages]));
+file_put_contents("config.json", json_encode(['version' => $version, 'languages' => $languages, 'generatedOn' => date('Y-m-d H:i:s')]));
 
 //Ddragon data, per language
 foreach($languages as $language){
