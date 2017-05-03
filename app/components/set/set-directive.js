010f77115dfe28a6df905359f54288e39563fceb
Rachid <rachid@actived.fr>
2017-05-03 09:36:25 +0200

    Fix #32: Downloading on Vivaldi browser

--- app/components/set/set-directive.js
+++ app/components/set/set-directive.js
@@ -359,7 +359,7 @@ angular.module('appLolIse.set.set-directive', ['ngFileUpload'])
                 //Send the zipped file
                 var filename = "Lol_Item_Set.zip";
                 console.log(bowser);
-                if(bowser.chrome || bowser.opera || bowser.firefox){
+                if(bowser.chrome || bowser.opera || bowser.firefox || bowser.vivaldi){
                     //For Chrome it works nicely
                     zip.generateAsync({type:"blob"}).then(function(blob){saveAs(blob, filename);});
                 }
